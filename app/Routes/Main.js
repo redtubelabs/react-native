d4695e26ded7055dd3a94b836e6d0bd4eca41991
Higor Alves <Goodyearalves@gmail.com>
2018-12-24 18:39:25 -0200

    App Init

--- app/Routes/Main.js
+++ app/Routes/Main.js
@@ -2,8 +2,30 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 
 import Home from '../Views/Home';
 
+import { purple } from '../utils/colors';
+
 const AppNavigator = createStackNavigator({
   Home: { screen: Home }
-});
+},
+  {
+    initialRouteName: 'Home',
+    gesturesEnabled: false,
+    defaultNavigationOptions: {
+      headerTitle: ('RedTube Labs'),
+      headerStyle: {
+        backgroundColor: purple,
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        color: 'white',
+        alignSelf: 'center',
+        textAlign: 'center',
+        justifyContent: "center",
+        alignSelf: "center",
+        width: '90%',
+      },
+    },
+
+  });
 
 export default createAppContainer(AppNavigator);
