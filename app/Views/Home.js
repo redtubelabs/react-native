45eb2bfa873baa0a2598001422e48d6b75b34ab3
Higor Alves <Goodyearalves@gmail.com>
2018-12-24 19:02:04 -0200

    Home screen

--- app/Views/Home.js
+++ app/Views/Home.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Button, Text, View, TextInput } from 'react-native';
+import { purple } from '../utils/colors';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchWord: '',
+    }
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData() {
+    fetch(`https://api-redtube-proxy.mybluemix.net/?search=${this.state.searchWord}`)
+      .then(res => res.json())
+      .then(res => console.warn(res))
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Search something porn...</Text>
+        <TextInput onChangeText={(text) => this.setState({ searchWord: text })} value={this.state.searchWord} style={styles.input} />
+        <View style={styles.button}>
+          <Button
+            title="Search"
+            color={purple}
+            accessibilityLabel="Search"
+            onPress={this.fetchData}
+          />
+        </View>
       </View>
     );
   }
@@ -19,14 +42,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  input: {
+    height: 40,
+    width: '90%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8
+  },
+  button: {
+    marginTop: 20,
+    width: '90%',
+    borderRadius: 20
+  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
